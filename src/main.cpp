5bc92aef505ddd4acc831d5b7d7691f16a959297
kikswosil <kikswosil91@gmail.com>
2025-09-24 14:12:25 +0200

    Program no longer puts garbage into outputFileName.

--- src/main.cpp
+++ src/main.cpp
@@ -7,34 +7,53 @@ void parseArgs(int argc, char* argv[], std::string& inputFileName, std::string& 
 void formatNumbers(std::ifstream& inputFile, std::function<std::string(const std::string)> formatFunc);
 std::string xlnToGcodeFormat(std::string line);
 
+void readfile(std::string filename, std::function<void(std::ifstream&, std::function<std::string(const std::string)>)> callback) {
+    std::ifstream file(filename);
+    if(!file.is_open()) {
+        std::cerr << "Could not find file: " << filename << std::endl;
+        system("exit");
+    }
+    formatNumbers(file, xlnToGcodeFormat);
+    file.close();
+}
+
 int main(int argc, char* argv[]) {
-    std::string inputFileName, outputFileName;
+    std::string inputFileName = "";
+    std::string outputFileName = "";
+    // std::string _;
 
-
+    // find filenames
     parseArgs(argc, argv, inputFileName, outputFileName);
 
     // open input file
-    std::ifstream inputFile(inputFileName);
-    // handle file open error
-    if (!inputFile) {
-        std::cerr << "Error opening input file: " << inputFileName << std::endl;
-        return 1;
-    }
-    // format numbers in the file
-    formatNumbers(inputFile, xlnToGcodeFormat);
+    std::cout << "output: " << outputFileName << std::endl;
+    // std::ifstream inputFile(inputFileName, std::ios::in);
+    readfile(inputFileName, formatNumbers); // temporary solution 
+    std::cout << "output: " << outputFileName << std::endl;
+    // // handle file open error
+    // if (!inputFile.is_open()) {
+    //     std::cerr << "Error opening input file: " << inputFileName << std::endl;
+    //     return 1;
+    // }
+    // // format numbers in the file
+    // formatNumbers(inputFile, xlnToGcodeFormat);
     // close file
-    inputFile.close();
+    // inputFile.close();
 
     std::cout << "input: " << inputFileName << ", output: " << outputFileName << std::endl;
     return 0;
 }
 
 void parseArgs(int argc, char* argv[], std::string& inputFileName, std::string& outputFileName) {
-    for (int i = 0; i < argc; ++i) {
+    for (int i = 1; i < argc; i++) {
+        std::cout << argv[i] << std::endl;
         if(std::string(argv[i]) == "-i" && i + 1 < argc) {
             inputFileName = argv[i + 1];
-        } else if(std::string(argv[i]) == "-o" && i + 1 < argc) {
+            // ++i;
+        } 
+        else if(std::string(argv[i]) == "-o" && i + 1 < argc) {
             outputFileName = argv[i + 1];
+            // ++i;
         }
     }
 }
